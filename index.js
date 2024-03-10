10015cb25139da34675566df3e1a71693e4f0435
Aditya Raj <adiraj1719@gmail.com>
2024-03-10 19:09:45 +0530

    Added Front End for Live USer Experience

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-import inquirer from 'inquirer';
+import express from "express";
 import qr from "qr-image";
 import fs from "fs";
+import { dirname, join } from "path";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
 
-inquirer
-    .prompt([
-        {
-            "message": "Enter the URL of your choice: ",
-            "name": "URL"
-        }
-    ])
-    .then((answers) => {
-        // console.log(answers);
-        const url = answers.URL;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
 
-        var qr_png = qr.image(url);
-        qr_png.pipe(fs.createWriteStream('new_qr.png'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(join(__dirname, "public")));
 
-        fs.writeFile('link.txt', url, (err) => {
-            if (err) throw err;
-            console.log('The file has been saved!');
-        });
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            // Something else went wrong
-        }
+app.post("/submit", (req, res) => {
+    const url = req.body.url;
+    console.log("Received URL:", url);
+
+    // Generate QR code and save it to file
+    const qr_png = qr.image(url);
+    const qrImagePath = join(__dirname, "public", "new_qr.png");
+    qr_png.pipe(fs.createWriteStream(qrImagePath));
+    fs.writeFile(join(__dirname, "public", "link.txt"), url, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
     });
+    res.locals.new_qr_exists = true;
+    res.render("home.ejs");
+});
 
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
 
-
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
